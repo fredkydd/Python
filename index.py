8445dcf0a8942ae25fdda244fced0769a46371c7
Fred Kydd <17003685+fredkydd@users.noreply.github.com>
2023-07-28 16:58:56 +0400

    Python print, input functions

---
+++ index.py
@@ -0,0 +1,21 @@
+# print's the same as JavaScript's console.log()
+print('Hello World! 0\nHello World! 1\nHello World! 2')
+print('Hello' + ' ' + 'Angela')
+print('Hello ' + 'Angela')
+print('Hello' + ' Angela')
+# This triple single or double quotes are used for multi line comments like JavaScaript's backtick
+print("""
+  Hello There Triple double quotes for multi-line comments
+  I'm the second line of the string
+  I'm the third line of the string
+""")
+
+print('''
+  Hello There Triple single quotes for multi-line comments
+  I'm the second line of the string
+  I'm the third line of the string
+''')
+
+# input is the same as JavaScript's prompt()
+input('How old are you ?')
+print('Hello ' + input('What is your first name?') + '!')
